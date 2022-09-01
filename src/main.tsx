1c3f891aea4c546ff1226eabca93e92958923e5d
DESKTOP-IDOC3K2\Tun <s3818475@rmit.edu.vn>
2022-09-01 21:41:48 +0700

    edit main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+//ReactDOM.createRoot(document.getElementById('root')!).render(
+//  <React.StrictMode>
+//    <Router basename="/">
+//      <App />
+//    </Router>
+//  </React.StrictMode>
+//)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+
+root.render(
   <React.StrictMode>
     <Router basename="/">
       <App />
     </Router>
-  </React.StrictMode>
+</React.StrictMode>
 )
+
